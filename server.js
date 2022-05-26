0c8b20a05b973e012e8b7b3414ceaed4745ace33
geovanefi <93933011+geovanefi@users.noreply.github.com>
2022-05-25 22:02:58 -0300

    Segunda parte de apredizagem do módulo NodeJS+EJS

--- server.js
+++ server.js
@@ -6,12 +6,48 @@ app.set("view engine", "ejs"); //avisa ao express que o html(view engine) sera e
 //criando rota - // get esta dentro do express
 //a const app vai chamar o endereço "/", vai chamar a funcao res (resposta) que por sua vez irá renderizar (render) o index que deve esta dentro de uma outra pasta views. 
 app.get("/", function (req, res) {
-    res.render("pages/index") // na separação de pastas fica views/pages/index.ejs - reduzindo fica pages/index
-})
+
+    const itens = [
+        { 
+            title: "V",
+            message: "ocê, sempre tão linda"
+        },
+        { 
+            title: "A",
+            message: "inda é o amor e o sentido"
+        },
+        { 
+            title: "N",
+            message: "esse mundo sem sentido."
+        },
+        { 
+            title: "E",
+            message: "ra estranho sem você,"
+        },
+        { 
+            title: "S",
+            message: "em graça e"
+        },
+        { 
+            title: "S",
+            message: "em cor."
+        },
+        { 
+            title: "A",
+            message: "gora é só amor e cor!"
+        },
+    ];
+
+    const subtitles = "Obrigado por existir!"
+    res.render("pages/index",{
+        descricao: itens,
+        subtitles: subtitles,
+    }); // na separação de pastas fica views/pages/index.ejs - reduzindo fica pages/index
+});
 
 app.get("/sobre", function (req, res) {
     res.render("pages/about")
-})
+});
 
 
 //criando porta para exibir - a porta nesse caso é 8080
